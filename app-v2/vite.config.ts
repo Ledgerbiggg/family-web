02f592ec7d656b6179f34b74def5c35c0a4dd86a
ledger <ledgerbiggg@gmail.com>
2024-12-17 22:17:57 +0800

    运行app2

--- app-v2/vite.config.ts
+++ app-v2/vite.config.ts
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import {resolve} from 'path'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8001/v1',
+                changeOrigin: true,
+                rewrite: (path) => {
+                    console.log('Rewriting path:', path); // 打印路径
+                    return path.replace(/^\/api/, '');
+                },
+                secure: false,
+            },
+        }
+
+    },
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src'),  // 设置 @ 为 src 目录
         },
     },
-    plugins: [vue()],
+    plugins: [vue(), vueDevTools(),],
 })
