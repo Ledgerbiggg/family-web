b23ac22aca892bc4ecf8796a42ab334ffc446572
ledger <123456789@qq.com>
2024-12-25 17:06:30 +0800

    swagger

--- docs/docs.go
+++ docs/docs.go
@@ -454,7 +454,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "日常生活.0",
+	Version:          "1.0",
 	Host:             "localhost:8001",
 	BasePath:         "/v1",
 	Schemes:          []string{},
